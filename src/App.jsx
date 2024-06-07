c30546760f5068607ca45572035c3f15f2043f04
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-07 12:49:12 -0700

    add Hero, Research, Booking, About import statements and component tags, add main element tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./pages/Hero";
+import Research from "./pages/Research";
+import Booking from "./pages/Booking";
+import About from "./pages/About";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <main>
       <Navbar />
+      <Hero />
+      <Research />
+      <Booking />
+      <About />
       <Footer />
-    </>
+    </main>
   );
 }
 
