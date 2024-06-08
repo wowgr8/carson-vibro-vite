12fe9abe8d9651e0e44e21be582b97d8cb6d0bf1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-07 23:23:58 -0700

    add expandNavbar useState for mobile view, react scroll install and mobileMenuLinks array

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const mobileMenuLinks = [
+    {
+      title: "Home",
+      path: "heroSection",
+    },
+    {
+      title: "Research",
+      path: "researchSection",
+    },
+    {
+      title: "Booking",
+      path: "bookingSection",
+    },
+    {
+      title: "About",
+      path: "aboutSection",
+    },
+  ];
+
   return <div>Navbar</div>;
 }
 
