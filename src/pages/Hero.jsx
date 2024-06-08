a8d018be2c71279655483a7917ee8bd24a7dc844
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-08 00:06:04 -0700

    add react scroll import, add Element tag with name attribute for hero page

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Element } from "react-scroll";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <Element name="hero" className="lg:py-16">
+      <div>Hero</div>
+    </Element>
+  );
 }
 
 export default Hero;
