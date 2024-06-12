b8a93871f0884e8050d0a05ff4d3b8a9fb84bdd3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-12 12:04:17 -0700

    add updated text

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -20,12 +20,12 @@ function Hero() {
       </video>
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 z-20">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          We invest in the world’s potential
+          Let go of you Mind. Let go of you body. Find your Self.
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
-        </p>
+        <h2 className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-600">
+          A fully immersive, therapeutic sound experience designed to promote
+          accelerated entry into deep meditative states.
+        </h2>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
             href="#"
