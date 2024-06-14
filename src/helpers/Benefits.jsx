4d5d19a751a7b04be1a4030788437dc00b7448b7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-13 18:39:37 -0700

    add BenefitsList array with id, image, and description key/values

---
+++ src/helpers/Benefits.jsx
@@ -0,0 +1,41 @@
+export const BenefitsList = [
+  {
+    id: 1,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_714_1024x1024.png?v=1679468036",
+    description: "Accelerates entry into deep meditative states.",
+  },
+  {
+    id: 2,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_716_1024x1024.png?v=1679468037",
+    description: "Improves body-mind communication for greater harmony.",
+  },
+  {
+    id: 3,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_716_copy_1024x1024.png?v=1679468037",
+    description:
+      "Promotes balance of the body through support of cellular homeostasis.",
+  },
+  {
+    id: 4,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_717_1024x1024.png?v=1679468037",
+    description: "Promotes sustained calmness and self-awareness.",
+  },
+  {
+    id: 5,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_718_1024x1024.png?v=1679468036",
+    description:
+      "Supports performance enhancement and recovery from physical activity.",
+  },
+  {
+    id: 6,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Layer_719_1024x1024.png?v=1679468036",
+    description:
+      "Encourages comfortable exploration of non-ordinary states of consciousness.",
+  },
+];
