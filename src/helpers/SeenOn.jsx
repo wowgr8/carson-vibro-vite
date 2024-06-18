fdcf31ab9177689a8405d46fe5dcd2b8ec29bcb8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-18 13:05:34 -0700

    add SeenOnList array with object key/values: id, image, and description

---
+++ src/helpers/SeenOn.jsx
@@ -0,0 +1,50 @@
+export const SeenOnList = [
+  {
+    id: 1,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Forbes_logo_1024x1024.png?v=1679570486",
+    description: "Forbes Magazine.",
+  },
+  {
+    id: 2,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/The_Times_Logo_1024x1024.png?v=1679570487",
+    description: "The Times Newspaper",
+  },
+  {
+    id: 3,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/FOX_logo_1024x1024.png?v=1679570486",
+    description: "FOX",
+  },
+  {
+    id: 4,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/New_York_Post_logo_1024x1024.png?v=1679570486",
+    description: "New York Post",
+  },
+  {
+    id: 5,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/L…ekly_logo_1024x1024_1_1024x1024.webp?v=1690271396",
+    description: "LA Weekly",
+  },
+  {
+    id: 6,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Wired_logo_1024x1024_1_1024x1024.webp?v=1690271340",
+    description: "Wired Magazine",
+  },
+  {
+    id: 7,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/M…aire_logo_1024x1024_1_1024x1024.webp?v=1690271297",
+    description: "Marie Claire",
+  },
+  {
+    id: 8,
+    image:
+      "//sagespacetechnologies.com/cdn/shop/files/Y…news_logo_1024x1024_1_1024x1024.webp?v=1690271247",
+    description: "Yahoo News",
+  },
+];
