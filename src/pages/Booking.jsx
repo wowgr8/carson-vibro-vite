0c39d4b50f9c6ba3b257b31184038e9741ff93bc
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-19 11:58:24 -0700

    add useEffect to handle Calendly script loading. Add Calendly widget to return

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Element } from "react-scroll";
 
 function Booking() {
+  useEffect(() => {
+    // Load Calendly script
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Element name="bookingSection">
-      <div>Booking</div>
+      <div className="flex flex-col items-center justify-center mt-5">
+        {/* <!-- Calendly inline widget begin --> */}
+        <div
+          className="calendly-inline-widget"
+          data-url="https://calendly.com/773ohh/vibe-thrive-session"
+          style={{ minWidth: "320px", height: "700px" }}
+        ></div>
+        {/* <!-- Calendly inline widget end --> */}
+      </div>
     </Element>
   );
 }
