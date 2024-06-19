f270eb0b1b45c406b4a4ac13af9c36069bc8f740
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-19 12:09:29 -0700

    add commented out Popup Text version of scheduling through calendly - WIP

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -29,3 +29,54 @@ function Booking() {
 }
 
 export default Booking;
+
+// BELOW IS A POPUP TEXT VERSION OF CALENDLY
+//
+//
+//
+//
+//
+
+// import React, { useEffect } from "react";
+
+// function BookingLink() {
+//   useEffect(() => {
+//     // Load Calendly CSS
+//     const link = document.createElement("link");
+//     link.href = "https://assets.calendly.com/assets/external/widget.css";
+//     link.rel = "stylesheet";
+//     document.head.appendChild(link);
+
+//     // Load Calendly script
+//     const script = document.createElement("script");
+//     script.src = "https://assets.calendly.com/assets/external/widget.js";
+//     script.async = true;
+//     document.body.appendChild(script);
+
+//     return () => {
+//       document.head.removeChild(link);
+//       document.body.removeChild(script);
+//     };
+//   }, []);
+
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     window.Calendly.initPopupWidget({
+//       url: 'https://calendly.com/773ohh/vibe-thrive-session?hide_gdpr_banner=1'
+//     });
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center mt-5">
+//       <a
+//         href="#"
+//         onClick={handleClick}
+//         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+//       >
+//         Schedule time with me
+//       </a>
+//     </div>
+//   );
+// }
+
+// export default BookingLink;
