614d712eb679627c3665a328cf1a3d3a5b704a94
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-06-20 10:52:50 -0700

    add temporary footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-250 bg-blue-950 flex flex-col justify-center items-center pt-12 mt-250">
+      <div className="flex">
+        <a
+          href="https://github.com/wowgr8"
+          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/cesar-aug-lopez/"
+          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="mailto:lopez.cesar.aug@gmail.com"
+          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
+        >
+          <AiOutlineMail />
+        </a>
+      </div>
+      <p className="text-white text-md">&copy; 2022 github.com/wowgr8</p>
+    </div>
+  );
 }
 
 export default Footer;
