34546ef1b9aa6691fd43c324504d839fa251e1ff
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-07-03 17:48:54 -0700

    add business name and address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,16 @@ function Footer() {
     <div className="bg-blue-950">
       <div className="max-w-screen-xl py-10 px-4 sm:px-6 dark:text-white text-gray-800 sm:flex justify-between mx-auto">
         {/* placeholder for location address to the left */}
-        <div className="p-5 sm:w-8/12 bg-red-500">
-          <p>ADDRESS</p>
+        <div className="p-5 sm:w-7/12 bg-red-500">
+          <h3 className="font-bold text-3xl dark:text-white text-gray-800 mb-4">
+            Vibe & Thrive
+          </h3>
+          <h3 className="text-xl dark:text-white text-gray-800 mb-4">
+            4204 SE Boise st. Portland Oregon 97206
+          </h3>
         </div>
         {/* placeholder for location map box */}
-        <div className="p-5 sm:w-4/12 bg-green-500">
+        <div className="p-5 sm:w-5/12 bg-green-500">
           <p>MAP</p>
         </div>
       </div>
