2549222dda83879facbf72652cb56160e4aa17cd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-07-05 14:36:16 -0700

    update section comments, remove unused commented code and unused imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
 
 function Footer() {
   return (
     <div className="bg-blue-950 border-t border-blue-900">
       <div className="max-w-screen-xl py-1 px-4 sm:px-6 dark:text-white text-gray-800 sm:flex justify-between mx-auto">
-        {/* placeholder for location address to the left */}
+        {/* Address and Logo section */}
         <div className="p-5 sm:w-7/12 flex flex-col items-center justify-center">
           <h3 className="font-bold text-5xl dark:text-white text-indigo-600 mb-4">
             Vibe & Thrive
@@ -15,13 +13,12 @@ function Footer() {
             4204 SE Boise st. Portland Oregon 97206
           </h3>
         </div>
-        {/* placeholder for location map box */}
+        {/* Map section */}
         <div className="p-5 sm:w-5/12">
           <div className="relative pt-[60%] overflow-hidden border-4 dark:border-yellow-300 border-indigo-600">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2796.8728214636294!2d-122.62123792350295!3d45.49250573154936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a070935d5ce5%3A0xefeed5e3b1ec70ef!2s4204%20SE%20Boise%20St%2C%20Portland%2C%20OR%2097206!5e0!3m2!1sen!2sus!4v1720203368890!5m2!1sen!2sus"
               className="absolute inset-0 w-full h-full"
-              // style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
@@ -37,26 +34,3 @@ function Footer() {
 }
 
 export default Footer;
-
-{
-  /* <div className="flex">
-        <a
-          href="https://github.com/wowgr8"
-          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
-        >
-          <BsGithub />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/cesar-aug-lopez/"
-          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
-        >
-          <BsLinkedin />
-        </a>
-        <a
-          href="mailto:lopez.cesar.aug@gmail.com"
-          className="text-yellow-300 hover:text-yellow-400 m-7 text-3xl"
-        >
-          <AiOutlineMail />
-        </a>
-      </div> */
-}
