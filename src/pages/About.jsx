b998e453be131cb4f6314458d9dcc0ea19535120
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-07-09 12:29:35 -0700

    add parent div with responsive styling for meet your practitioner text section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,9 @@ function About() {
       name="aboutSection"
       className="relative bg-white overflow-hidden mt-16"
     >
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"></div>
+      </div>
       <div>About</div>
     </Element>
   );
