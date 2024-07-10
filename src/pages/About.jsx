2d284a7d4fd8da433cad90c5eb9b22fd6c8c4760
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-07-10 13:12:19 -0700

    add transparent svg - WIP

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,15 +9,18 @@ function About() {
       className="relative bg-white overflow-hidden mt-16"
     >
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-red-500 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="relative z-10 pb-8 bg-red-300 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-24 text-white transform translate-x-1/2"
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-24 transform translate-x-1/2 text-transparent"
             fill="currentColor"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
             aria-hidden="true"
           >
-            <polygon points="50,0 100,0 50,100 0,100"></polygon>
+            <polygon
+              points="50,0 100,0 50,100 0,100"
+              // style={{ fill: "inherit" }}
+            ></polygon>
           </svg>
 
           <div className="pt-1"></div>
