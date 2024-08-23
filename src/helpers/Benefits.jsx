b5e1026d8aeaff80b95a426ce960986387c09941
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-08-23 15:29:29 -0700

    add new descriptions for each benefit.

--- src/helpers/Benefits.jsx
+++ src/helpers/Benefits.jsx
@@ -3,26 +3,26 @@ export const BenefitsList = [
     id: 1,
     image:
       "//sagespacetechnologies.com/cdn/shop/files/Layer_714_1024x1024.png?v=1679468036",
-    description: "Accelerates entry into deep meditative states.",
+    description: "Easing of physical and emotional tension.",
   },
   {
     id: 2,
     image:
       "//sagespacetechnologies.com/cdn/shop/files/Layer_716_1024x1024.png?v=1679468037",
-    description: "Improves body-mind communication for greater harmony.",
+    description: "Stress relief.",
   },
   {
     id: 3,
     image:
       "//sagespacetechnologies.com/cdn/shop/files/Layer_716_copy_1024x1024.png?v=1679468037",
     description:
-      "Promotes balance of the body through support of cellular homeostasis.",
+      "Promoting balance of the body and mind through the support of cellular homeostasis.",
   },
   {
     id: 4,
     image:
       "//sagespacetechnologies.com/cdn/shop/files/Layer_717_1024x1024.png?v=1679468037",
-    description: "Promotes sustained calmness and self-awareness.",
+    description: "Acceleration into deep meditative states.",
   },
   {
     id: 5,
@@ -35,7 +35,6 @@ export const BenefitsList = [
     id: 6,
     image:
       "//sagespacetechnologies.com/cdn/shop/files/Layer_719_1024x1024.png?v=1679468036",
-    description:
-      "Encourages comfortable exploration of non-ordinary states of consciousness.",
+    description: "Elevated mood.",
   },
 ];
