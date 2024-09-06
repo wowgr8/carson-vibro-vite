ea40c0c12a7216281a139a4c14e3e9882e574083
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-09-06 15:38:04 -0700

    add second phenomena - Cymatics

--- src/helpers/ThreeMainPhenomena.jsx
+++ src/helpers/ThreeMainPhenomena.jsx
@@ -6,8 +6,9 @@ export const PhenomenaList = [
       "Brainwave entrainment is a method used to stimulate the brain into entering a specific state by using pulsing sounds, lights, or electromagnetic fields. Different conscious states are associated with distinct dominant brainwave frequencies. At Vibe’n Thrive, we primarily focus on the theta and alpha brainwave states. Our goal is to guide your tired brain out of the monotonous beta state and into a more relaxed, rejuvenating state where true restoration can occur.",
   },
   {
-    id: 1,
-    title: "Brain Wave Entrainment",
-    description: ".",
+    id: 2,
+    title: "Cymatics",
+    description:
+      "Cymatics is the study of sound - wave phenomena- through liquid and loose particulate material. Since the body is about 70% water, Vibroacoustic therapy utilizes the idea of cymatics to create movement and has the ability to restructure cells and DNA in a much more harmonious way.",
   },
 ];
