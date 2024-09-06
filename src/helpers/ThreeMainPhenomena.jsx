76f48b0bf27dc0eed68cb9b35b17584da4b62d22
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-09-06 15:35:22 -0700

    add first phenomena

---
+++ src/helpers/ThreeMainPhenomena.jsx
@@ -0,0 +1,13 @@
+export const PhenomenaList = [
+  {
+    id: 1,
+    title: "Brain Wave Entrainment",
+    description:
+      "Brain wave entrainment Is a method to stimulate the brain into entering a specific state by using a pulsing sound, light, or electromagnetic field. Different conscious states can be associated with different dominant brainwave frequencies. The main brain wave states that we deal with at vibe’n thrive are theta and alpha. We work to get your tired brain out of the monotonous beta state and into a more relaxed rejuvenating state where restoration can take place. ",
+  },
+  {
+    id: 1,
+    title: "Brain Wave Entrainment",
+    description: ".",
+  },
+];
