746fe5c8b2121675ef386c877f578c6e8f75c2aa
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-09-14 10:34:11 -0700

    add typo fix for brainwave entrainment

--- src/helpers/ThreeMainPhenomena.jsx
+++ src/helpers/ThreeMainPhenomena.jsx
@@ -1,7 +1,7 @@
 export const PhenomenaList = [
   {
-    id: "Brain Wave Entrainment",
-    title: "Brain Wave Entrainment",
+    id: "Brainwave Entrainment",
+    title: "Brainwave Entrainment",
     description:
       "Brainwave entrainment is a method used to stimulate the brain into entering a specific state by using pulsing sounds, lights, or electromagnetic fields. Different conscious states are associated with distinct dominant brainwave frequencies. At Vibe’n Thrive, we primarily focus on the theta and alpha brainwave states. Our goal is to guide your tired brain out of the monotonous beta state and into a more relaxed, rejuvenating state where true restoration can occur.",
   },
