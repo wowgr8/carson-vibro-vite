1b7d1b9d8869d8c0581804d75efa32018e2192d3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-09-17 12:08:54 -0700

    add initial file for quotes banner

---
+++ src/components/QuotesBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function QuotesBanner() {
+  quotes_data = [
+    {
+      id: 1,
+      quote: "",
+      author: "",
+    },
+  ];
+
+  return <div>QuotesBanner</div>;
+}
+
+export default QuotesBanner;
